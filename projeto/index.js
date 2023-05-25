93b33ddd0b8337b865c543df3e87c8d4d31d28c1
DavidTMaciel <davidmacieljr122@gmail.com>
2023-05-25 19:31:06 -0300

    Adicionando o Body Parser e configurando rota

--- projeto/index.js
+++ projeto/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { engine } = require ('express-handlebars');
+const bodyParser = require('body-parser')
 
 const app = express();
 
@@ -8,7 +9,9 @@ const app = express();
     app.engine('handlebars', engine());
         app.set("view engine", "handlebars")
         app.set('views', 'views');
-
+    //Body Parser
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
     //Conexão com o banco de dados MySql;
     const Sequelize = require('sequelize');
     const sequelize = new Sequelize('sistemadecadastro','root', '3003',{
@@ -20,10 +23,13 @@ const app = express();
 
     app.get('/cad', function (req, res){
         res.render('formulario');
-    })
+    });
+    app.post('/add', function (req, res){
+        res.send("Texto: "+req.body.titulo+"</br>conteudo "+req.body.conteudo);
+    });
 
 
 app.listen(8081, function () {
     console.log("Servidor Rodando na url http://localhost:8081");
-})
+});
 
