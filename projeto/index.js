19ea1138152bbf90de11986990210725adc69149
DavidTMaciel <davidmacieljr122@gmail.com>
2023-05-27 11:16:54 -0300

    Adicionando botão deletar

--- projeto/index.js
+++ projeto/index.js
@@ -17,11 +17,9 @@ const Post = require('./Models/Post');
     
     app.get('/', (req, res) =>{
         //Passando os dados da tabela para a pagina Home
-        Post.findAll().then(function(posts){
-            res.render('home', {
-                posts: posts
-            });
-        });
+        Post.findAll({order: [['id', 'DESC']]}).then(function(posts){
+            res.render('home', {posts: posts})}
+            );
         
     });
 
@@ -40,6 +38,12 @@ const Post = require('./Models/Post');
        })
     });
     
+
+    app.get('/deletar/:id', function (req, res){
+        Post.destroy({where: {'id': req.params.id}}).then(function (){
+            res.send('Postagem deletada com sucesso');
+        }
+    )})
 
 
 app.listen(8081, function () {
